841adad506f7be8fe64747d6bcbf0d2466e4c47b
paulmorio <pms69@cam.ac.uk>
2020-02-06 17:37:44 +0000

    [Feature] Trainer that uses an in memory corpus to load data quicker

--- geometric2dr/embedding_methods/trainer.py
+++ geometric2dr/embedding_methods/trainer.py
@@ -7,7 +7,7 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 # Internal 
-from embedding_methods.data_reader import Corpus
+from embedding_methods.data_reader import Corpus, InMemoryCorpus
 from embedding_methods.skipgram import Skipgram
 from embedding_methods.utils import get_files, get_class_labels, get_class_labels_tuples, save_graph_embeddings
 
@@ -18,6 +18,59 @@ class Trainer(object):
 	def __init__(self, corpus_dir, extension, max_files, output_fh, emb_dimension=128, batch_size=32, epochs=100, initial_lr=1e-3, min_count=1):
 		self.corpus = Corpus(corpus_dir, extension, max_files, min_count)
 		self.dataloader = DataLoader(self.corpus, batch_size, shuffle=False, num_workers=0, collate_fn = self.corpus.collate)
+		
+		self.corpus_dir = corpus_dir
+		self.extension = extension
+		self.max_files = max_files
+		self.output_fh = output_fh
+		self.emb_dimension = emb_dimension
+		self.batch_size = batch_size
+		self.epochs = epochs
+		self.initial_lr = initial_lr
+		self.min_count = min_count
+
+		self.num_targets = self.corpus.num_graphs
+		self.vocab_size = self.corpus.num_subgraphs
+
+		self.skipgram = Skipgram(self.num_targets, self.vocab_size, self.emb_dimension)
+
+		if torch.cuda.is_available():
+			self.device = torch.device("cuda")
+			self.skipgram.cuda()
+		else:
+			self.device = torch.device("cpu")
+
+	def train(self):
+		for epoch in range(self.epochs):
+			print("### Epoch: " + str(epoch))
+			optimizer = optim.SparseAdam(self.skipgram.parameters(), lr=self.initial_lr)
+			scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, len(self.dataloader))
+
+			running_loss = 0.0
+			for i, sample_batched in enumerate(tqdm(self.dataloader)):
+
+				if len(sample_batched[0]) > 1:
+					pos_target = sample_batched[0].to(self.device)
+					pos_context = sample_batched[1].to(self.device)
+					neg_context = sample_batched[2].to(self.device)
+
+					optimizer.zero_grad()
+					loss = self.skipgram.forward(pos_target, pos_context, neg_context) # the loss is integrated into the forward function
+					loss.backward()
+					optimizer.step()
+					scheduler.step()
+
+					running_loss = running_loss * 0.9 + loss.item() * 0.1
+			print(" Loss: " + str(running_loss))
+
+		final_embeddings = self.skipgram.target_embeddings.weight.cpu().data.numpy()
+		save_graph_embeddings(self.corpus, final_embeddings, self.output_fh)
+
+
+class InMemoryTrainer(object):
+	def __init__(self, corpus_dir, extension, max_files, output_fh, emb_dimension=128, batch_size=32, epochs=100, initial_lr=1e-3, min_count=1):
+		self.corpus = InMemoryCorpus(corpus_dir, extension, max_files, min_count)
+		self.dataloader = DataLoader(self.corpus, batch_size, shuffle=True, num_workers=0, collate_fn = self.corpus.collate)
 		
 		self.corpus_dir = corpus_dir
 		self.extension = extension
