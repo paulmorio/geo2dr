03a73a254fca43705cb5281c3091efa3b7483051
paulmorio <pms69@cam.ac.uk>
2020-02-18 19:44:29 +0000

    [Examples] DGK shortest paths with inmemory datasets and correct class label to observation

--- examples/dgk_shortest_paths.py
+++ examples/dgk_shortest_paths.py
@@ -8,7 +8,7 @@ import numpy as np
 
 import geometric2dr.embedding_methods.utils as utils
 from geometric2dr.decomposition.shortest_path_patterns import sp_corpus
-from geometric2dr.embedding_methods.skipgram_trainer import Trainer
+from geometric2dr.embedding_methods.skipgram_trainer import InMemoryTrainer
 from geometric2dr.embedding_methods.classify import cross_val_accuracy_precomputed_kernel_matrix
 
 # Input data paths
@@ -30,8 +30,8 @@ extension = ".spp"
 # Step 2
 # Train a skipgram (w. Negative Sampling) model to learn distributed representations of the subgraph patterns
 ############
-trainer = Trainer(corpus_dir=corpus_data_dir, extension=extension, max_files=0, window_size=10, output_fh=output_embedding_fh,
-				  emb_dimension=32, batch_size=128, epochs=5, initial_lr=0.1,
+trainer = InMemoryTrainer(corpus_dir=corpus_data_dir, extension=extension, max_files=0, window_size=10, output_fh=output_embedding_fh,
+				  emb_dimension=50, batch_size=128, epochs=10, initial_lr=0.1,
 				  min_count=1)
 trainer.train()
 final_subgraph_embeddings = trainer.skipgram.give_target_embeddings()
@@ -53,12 +53,14 @@ for i in range(len(vocabulary)):
 		M[i][j] = np.dot(final_subgraph_embeddings[trainer.corpus._subgraph_to_id_map[str(vocabulary[i])]], final_subgraph_embeddings[trainer.corpus._subgraph_to_id_map[str(vocabulary[j])]])
 K = (P.dot(M)).dot(P.T)
 
-
 ###########
 # Step 4
 # Use some kernel method, such as an SVM to compute classifications on the graph kernel matrix
 ###########
 class_labels_fname = "data/"+ dataset + ".Labels"
-kernel_row_x_id, kernel_row_y_id = utils.get_kernel_matrix_row_idx_with_class(trainer.corpus, extension, graph_files, class_labels_fname)
+xylabels = utils.get_class_labels_tuples(graph_files,class_labels_fname)
+xylabels.sort(key=lambda tup: tup[0])
+kernel_row_x_id, kernel_row_y_id = zip(*xylabels)
+
 acc, std = cross_val_accuracy_precomputed_kernel_matrix(K, kernel_row_y_id)
 print ('#... Accuracy score: %0.3f, Standard deviation: %0.3f' % (acc, std))
