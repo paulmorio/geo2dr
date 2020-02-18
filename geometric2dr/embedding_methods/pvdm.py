a993c77b1fd51aec72a8fc0d0b19547c1cc98a49
paulmorio <pms69@cam.ac.uk>
2020-02-18 20:04:38 +0000

    [Testing] Using classical definition of PV-DM for testing and AWE replication, as results do not match their high results in their paper

--- geometric2dr/embedding_methods/pvdm.py
+++ geometric2dr/embedding_methods/pvdm.py
@@ -11,7 +11,6 @@ import numpy as np
 from torch.nn import init
 
 
-
 class PVDM(nn.Module):
     """
     PyTorch implmentation of the PVDM as in Le and Mikolov.
@@ -39,34 +38,34 @@ class PVDM(nn.Module):
 
     def forward(self, pos_graph_emb, pos_context_target, pos_contexts, pos_negatives):
         # Be aware pos_contexts is typically several context embeddings
+        emb_target_graph = self.target_embeddings(pos_graph_emb)
+        mean_contexts_subgraphs = torch.sum(self.context_embeddings(pos_contexts), dim=1)
+
+        stack_target_contexts = torch.cat((emb_target_graph, mean_contexts_subgraphs), dim=1)
+        h = self.linear1(stack_target_contexts)
+        h = self.activation1(h)
+        h = self.linear2(h)
+        out = self.activation2(h)
+        return out
+
+        # Negative sampling PV-DM
         # emb_target = self.target_embeddings(pos_graph_emb)
         # mean_contexts = torch.sum(self.context_embeddings(pos_contexts), dim=1)
-        # emb_context_target = self.context_embeddings(pos_context_target)
+        # emb_context_target = self.output_layer(pos_context_target)
+        # emb_negative_targets = self.output_layer(pos_negatives)
 
+        # # Concat graph embedding with contexts embeddings
         # stack_target_contexts = torch.cat((emb_target, mean_contexts), dim=1)
-        # h = self.linear1(stack_target_contexts)
-        # h = self.activation1(h)
-        # h = self.linear2(h)
-        # out = self.activation2(h)
-        # return out
 
-        emb_target = self.target_embeddings(pos_graph_emb)
-        mean_contexts = torch.sum(self.context_embeddings(pos_contexts), dim=1)
-        emb_context_target = self.output_layer(pos_context_target)
-        emb_negative_targets = self.output_layer(pos_negatives)
+        # objective = torch.sum(torch.mul(stack_target_contexts, emb_context_target), dim=1) # mul is elementwise multiplication
+        # objective = torch.clamp(objective, max=10, min=-10)
+        # objective = -F.logsigmoid(objective)
 
-        # Concat graph embedding with contexts embeddings
-        stack_target_contexts = torch.cat((emb_target, mean_contexts), dim=1)
+        # neg_objective = torch.bmm(emb_negative_targets, stack_target_contexts.unsqueeze(2)).squeeze()
+        # neg_objective = torch.clamp(neg_objective, max=10, min=-10)
+        # neg_objective = -torch.sum(F.logsigmoid(-neg_objective), dim=1)
 
-        objective = torch.sum(torch.mul(stack_target_contexts, emb_context_target), dim=1) # mul is elementwise multiplication
-        objective = torch.clamp(objective, max=10, min=-10)
-        objective = -F.logsigmoid(objective)
-
-        neg_objective = torch.bmm(emb_negative_targets, stack_target_contexts.unsqueeze(2)).squeeze()
-        neg_objective = torch.clamp(neg_objective, max=10, min=-10)
-        neg_objective = -torch.sum(F.logsigmoid(-neg_objective), dim=1)
-
-        return torch.mean(objective+neg_objective)
+        # return torch.mean(objective+neg_objective)
 
     def give_target_embeddings(self):
         embedding = self.target_embeddings.weight.cpu().data.numpy()
