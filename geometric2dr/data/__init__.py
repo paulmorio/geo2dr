6080736f3b450028f9676558f8804a8848b26a0c
paulmorio <pms69@cam.ac.uk>
2020-03-22 21:00:36 +0000

    [Docs] Refactoring for Sphinx

--- geometric2dr/data/__init__.py
+++ geometric2dr/data/__init__.py
@@ -1,3 +1,1 @@
-# init file to organise package
-# files in this module primarily transform different common datasets into gexf formats more amenable to our process.
 from .dortmund_formatter import DortmundGexf
